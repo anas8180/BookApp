55e24f5cc921e7f306a18deb7cce91a0ef9e6613
Prasannavenkatesh1 <iphonedinesh30@gmail.com>
2017-06-14 17:34:22 +0530

    Completed the பதிப்பாளர்கள்” and “புத்தக வகைகள்" and added the Profile and about us page.

---
+++ BookApp/AboutUsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutUsViewController.swift
+//  BookApp
+//
+//  Created by Asaraa on 6/14/17.
+//  Copyright © 2017 oms. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+     @IBOutlet var aboutusLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookApp/ProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileViewController.swift
+//  BookApp
+//
+//  Created by Asaraa on 6/14/17.
+//  Copyright © 2017 oms. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet var profileImg: UIImageView!
+    @IBOutlet var profileName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
