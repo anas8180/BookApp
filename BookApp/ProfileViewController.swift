4f5c7542fd01deea295a8b933529b45274e47d85
Logeshw <logeshwaran@whalts.com>
2017-06-17 14:20:29 +0530

    Removed >>>

--- BookApp/ProfileViewController.swift
+++ BookApp/ProfileViewController.swift
@@ -12,10 +12,8 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet var profileImg: UIImageView!
     @IBOutlet var profileName: UILabel!
-<<<<<<< HEAD
-    
-=======
->>>>>>> c0f3812e54d90e2bd15804b975ae05bf5de45be3
+ 
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
